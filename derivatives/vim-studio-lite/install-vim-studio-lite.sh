3b7506957cb726fc2aa2dc6dd5f04119837e64f9
Max O'Cull <maxattax97@gmail.com>
2017-08-27 17:52:20 -0400

    Fix shell functions

--- derivatives/vim-studio-lite/install-vim-studio-lite.sh
+++ derivatives/vim-studio-lite/install-vim-studio-lite.sh
@@ -1,40 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-log "Installing Dein plugin manager ..."
-if [ ! -z "$(command -v curl)" ]; then
-    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh; check
-elif [ ! -z "$(command -v wget)" ]; then
-    wget https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -O /tmp/installer.sh; check
-else
-    error "Neither wget nor curl are installed -- can not complete installation"
-    exit 1
-fi
-
-if [ -s /tmp/installer.sh ]; then
-    sh /tmp/installer.sh "$HOME/.dein"; check
-    rm /tmp/installer.sh; check
-else
-    error "Download for Dein failed"
-    exit 1
-fi
-
-cd "$HOME" || exit
-if [ -s "$HOME/.vimrc" ]; then
-    log "Backing up old .vimrc to $HOME/.vimrc.bak ..."
-    mv "$HOME/.vimrc" "$HOME/.vimrc.bak"; check
-fi
-
-log "Linking new .vimrc ..."
-ln -s "$SCRIPT_DIR/vim-studio-lite.vim" "$HOME/.vimrc"; check
-
-log "Installing plugins ..."
-vim +qa; check
-
-success "Installation complete for Vim Studio Lite"
-success "Execute \`vim\` to begin an epic experience"
-
 RED='\e[0;31m'
 LIGHT_RED='\e[1;31m'
 YELLOW='\e[1;33m'
@@ -88,3 +53,37 @@ error()
     echo -e "${RED}${BLINK}=(${checkCount}/${checkTotal})=[ ${*}${CLEAR}"
 }
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+log "Installing Dein plugin manager ..."
+if [ ! -z "$(command -v curl)" ]; then
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh; check
+elif [ ! -z "$(command -v wget)" ]; then
+    wget https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -O /tmp/installer.sh; check
+else
+    error "Neither wget nor curl are installed -- can not complete installation"
+    exit 1
+fi
+
+if [ -s /tmp/installer.sh ]; then
+    sh /tmp/installer.sh "$HOME/.dein"; check
+    rm /tmp/installer.sh; check
+else
+    error "Download for Dein failed"
+    exit 1
+fi
+
+cd "$HOME" || exit
+if [ -s "$HOME/.vimrc" ]; then
+    log "Backing up old .vimrc to $HOME/.vimrc.bak ..."
+    mv "$HOME/.vimrc" "$HOME/.vimrc.bak"; check
+fi
+
+log "Linking new .vimrc ..."
+ln -s "$SCRIPT_DIR/vim-studio-lite.vim" "$HOME/.vimrc"; check
+
+log "Installing plugins ..."
+vim +qa; check
+
+success "Installation complete for Vim Studio Lite"
+success "Execute \`vim\` to begin an epic experience"
